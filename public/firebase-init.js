e94585f6888e9345e20b5cd6eeaf0d8a31cc1818
corneliuspam <corneliuspam8736@gmail.com>
2026-01-16 20:39:18 +0100

    Create firebase-init.js

---
+++ public/firebase-init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
